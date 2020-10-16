bf66f68e7b2328c4460c57da4d425f13baf2a18a
Pascal Pascarella <pascalpascarella86@gmail.com>
2020-10-16 05:10:08 -0500

    Initialize Create.cshtml view, add constructor & member var to controller (ln 15, ln 18), implement add and save methods for db (ln 48)

--- Superhero-Creator2/Controllers/SuperheroesController.cs
+++ Superhero-Creator2/Controllers/SuperheroesController.cs
@@ -4,12 +4,22 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Superhero_Creator2.Data;
 using Superhero_Creator2.Models;
 
 namespace Superhero_Creator2.Controllers
 {
 	public class SuperheroesController : Controller
 	{
+		// Member Variable
+		private ApplicationDbContext _db;		// Represents DB anywhere inside of class
+
+		// Constructor
+		public SuperheroesController(ApplicationDbContext db)		// Passes the project's INSTANCE into the controller
+		{
+			_db = db;		// Assigns input to _db member variable on line 14
+		}
+
 		// GET: SuperheroesController
 		public ActionResult Index()
 		{
@@ -31,10 +41,12 @@ namespace Superhero_Creator2.Controllers
 		// POST: SuperheroesController/Create
 		[HttpPost]
 		[ValidateAntiForgeryToken]
-		public ActionResult Create(Superhero collection)
+		public ActionResult Create(Superhero superhero)
 		{
 			try
 			{
+				_db.Superheroes.Add(superhero);   // Takes captured superhero data, adds to table in our DB
+				_db.SaveChanges();
 				return RedirectToAction(nameof(Index));
 			}
 			catch
@@ -52,7 +64,7 @@ namespace Superhero_Creator2.Controllers
 		// POST: SuperheroesController/Edit/5
 		[HttpPost]
 		[ValidateAntiForgeryToken]
-		public ActionResult Edit(int id, Superhero collection)
+		public ActionResult Edit(int id, Superhero superhero)
 		{
 			try
 			{
@@ -73,7 +85,7 @@ namespace Superhero_Creator2.Controllers
 		// POST: SuperheroesController/Delete/5
 		[HttpPost]
 		[ValidateAntiForgeryToken]
-		public ActionResult Delete(int id, Superhero collection)
+		public ActionResult Delete(int id, Superhero superhero)
 		{
 			try
 			{
