3cfb5db85d7485a1fe0ea2e22ea22c5b972ffa27
Pascal Pascarella <pascalpascarella86@gmail.com>
2020-10-16 03:55:56 -0500

    Add DbSet table prop to ln 14

--- Superhero-Creator2/Data/ApplicationDbContext.cs
+++ Superhero-Creator2/Data/ApplicationDbContext.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Superhero_Creator2.Models;
 
 namespace Superhero_Creator2.Data
 {
+	// Represents DB inside application
 	public class ApplicationDbContext : IdentityDbContext
 	{
+		// Table in DB
+		public DbSet<Superhero> Superheroes { get; set; }
+
 		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
 				: base(options)
 		{
