6da8ad05d10127dae0b426d57489e49b14824416
Pascal Pascarella <pascalpascarella86@gmail.com>
2020-10-16 03:48:15 -0500

    Create Superhero.cs model with appropriate props and Display attributes

--- Superhero-Creator2/Models/Superhero.cs
+++ Superhero-Creator2/Models/Superhero.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,23 @@ namespace Superhero_Creator2.Models
 {
 	public class Superhero
 	{
+		// Primary Key
+		[Key]
+		public int Id { get; set; }
+
+		[Display(Name ="Hero Name")]
+		public string HeroName { get; set; }
+
+		[Display(Name = "Alias")]
+		public string Alias { get; set; }
+
+		[Display(Name = "Primary Superpower")]
+		public string SuperPowerPrimary { get; set; }
+
+		[Display(Name = "Secondary Superpower")]
+		public string SuperPowerSecondary { get; set; }
+
+		[Display(Name = "Catchphrase")]
+		public string Catchphrase { get; set; }
 	}
 }
